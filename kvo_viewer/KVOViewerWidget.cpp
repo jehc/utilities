06429516479c33f8c47b6c57c36768813d16f4ec
Kevin James Matzen <kmatzen@angelus.cs.cornell.edu>
2011-05-29 15:51:59 -0400

    Fixed up some portability issues with kvo.

---
+++ kvo_viewer/KVOViewerWidget.cpp
@@ -0,0 +1,26 @@
+#include "KVOViewerWidget.h"
+#include <cassert>
+
+void
+KVOViewerWidget::initializeGL()
+{
+  assert (0);
+}
+
+void
+KVOViewerWidget::resizeGL (int width, int height)
+{
+  assert (0);
+}
+
+void
+KVOViewerWidget::paintGL ()
+{
+  assert (0);
+}
+
+void
+KVOViewerWidget::keyPressEvent (QKeyEvent *e)
+{
+  assert (0);
+}
