b71925d545b85caecc9cf6a687bd7a711165fa83
Kevin Matzen <kmatzen@hydra101.cs.cornell.edu>
2011-06-04 22:11:52 -0400

    Updated KVO to contain color information.

--- kvo/kvo.h
+++ kvo/kvo.h
@@ -6,19 +6,26 @@
 
 class KVO
 {
-std::vector<std::vector<std::vector<float> > > data;
+public:
+struct Color
+{
+  float r, g, b, a;
+  Color () : r(0), g(0), b(0), a(0) { }
+};
+private:
+std::vector<std::vector<std::vector<Color> > > data;
 float voxel_size;
 Eigen::Vector3f translation;
 public:
 KVO ( int a, int b, int c, float voxel_size, const Eigen::Vector3f & translation )
-  : data ( std::vector<std::vector<std::vector<float> > > ( a, std::vector<std::vector<float> > ( b,std::vector<float > ( c ) ) ) )
+  : data ( std::vector<std::vector<std::vector<Color> > > ( a, std::vector<std::vector<Color> > ( b,std::vector<Color> ( c ) ) ) )
   , voxel_size ( voxel_size )
   , translation ( translation )
 {
 }
 void save ( const std::string & );
 static KVO load ( const std::string & );
-std::vector<std::vector<float> > & operator [] ( int i )
+std::vector<std::vector<Color> > & operator [] ( int i )
 {
   return data [i];
 }
