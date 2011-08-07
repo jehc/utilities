c90367122b81c45335609d01804bf42db5788a14
Kevin James Matzen <kmatzen@cs.cornell.edu>
2011-08-07 13:03:12 -0400

    Forgot this one

--- BundleFile/BundleView.h
+++ BundleFile/BundleView.h
@@ -13,6 +13,7 @@ class BundleView
   BundleView () {}
   BundleView (int camera, int key, float x, float y):camera(camera),key(key),x(x),y(y){}
   inline int GetCamera() const { return camera; }
+  inline int GetKey() const { return key; }
   inline float GetX() const { return x; }
   inline float GetY() const { return y; }
 };
